81ef98edf15b8d941b59d34529e0dbce56ccf746
Ngocruby <39007362+Ngocruby@users.noreply.github.com>
2018-06-18 14:43:09 +0200

    neu

--- src/index.js
+++ src/index.js
@@ -4,16 +4,47 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-// const content = 'HEllO World'
-// const myClassName = 'container'
-//const element = <div className = {myClassName}> {content} </div> // JSX- Js+XML
+//test1
+const content = 'HEllO World'
+const myClassName = 'behaelter'
+const element1 = <div className = {myClassName}> {content} </div> // JSX- Js+XML
+ReactDOM.render(element1 , document.getElementById('root1'));
+
+//test2
 const props = {
     className: 'container',
     children: 'Hello',
 }
-const element =<div {...props} />
+const a = <div {...props} /> // Props la properties cua 1 component
+const element2 =<div {...props} />
 
-ReactDOM.render(element , document.getElementById('root'));
+ReactDOM.render(element2 , document.getElementById('root2'));
 
+//test3
+const rootElement = document.getElementById('root3')
+const Message = props => <div> {props.children}</div> //Error funktion
+const element = (
+    <div className = "container">
+        <Message> Hello World 
+            <Message> Goodbye World </Message> 
+            {/* <Message msg="Goodbye World"/> */}
+        </Message>
+        
+    </div>
+)
+ReactDOM.render(element, rootElement)
+
+//test 4 
+function SayHello(props){
+    return(
+        <div>
+            Hello {props.firstName}{props.lastName}!
+        </div>
+    )
+}
+ReactDOM.render(
+    <SayHello firstName = {true}/>,
+    document.getElementById('root4')
+)
 // ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
